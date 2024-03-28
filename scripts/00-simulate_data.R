d728299e729e0b61770d7b8d183309ed020e526c
Mingjia Chen <mingjia.chen@mail.utoronto.ca>
2024-03-28 09:55:51 -0400

    updated simulated data and test cases

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,32 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates the raw dataset for code testing
+# Author: Mingjia Chen
+# Date: 28 March 2023
+# Contact: mingjia.chen@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: R
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+simulated_data <-
+  tibble(
+    mental_health =runif(n = 1000,min = 0,max = 7) |> floor(),
+    physical_health = runif(n = 1000,min = 0,max = 7) |> floor(),
+)
 
+# expected that mental_health is only between 0 to 6
+# expected that  physical_health is only between 0 to 6
 
+#### Add tests for simulated data ####
 
+# expected that mental_health is only between 0 to 6
+any(unique(simulated_data$mental_health)) == c(0:6)
+min(simulated_data$mental_health) == 0
+max(simulated_data$mental_health) == 6
+# expected that  physical_health is only between 0 to 6
+any(unique(simulated_data$physical_health)) == c(0:6)
+min(simulated_data$physical_health) == 0
+max(simulated_data$physical_health) == 6
